4d5e61874542cae2e71f6d84d25f3073f14cb9a5
Sergio López Rey <sergiolopezdanielrey@gmail.com>
2024-04-12 22:53:00 -0300

    fixes

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,113 +1,113 @@
-import { useEffect, useState } from "react";
-import Button from "../utilities/Button";
-import Icon from "../utilities/Icon";
-import playImage from "../../assets/images/play-white.png";
-import pauseImage from "../../assets/images/pause-white.png";
-import fastForward from "../../assets/images/forward-white.png";
-import settingsIcon from "../../assets/images/settings-white.png";
-import BellSound from "../../assets/sounds/Bellsound.mp3";
-import Modal from "../utilities/Modal";
-const bellMusic = new Audio(BellSound);
-let tiempoDeInicio = Date.now();
+import { useEffect, useState } from 'react'
+import Button from '../utilities/Button'
+import Icon from '../utilities/Icon'
+import playImage from '../../assets/images/play-white.png'
+import pauseImage from '../../assets/images/pause-white.png'
+import fastForward from '../../assets/images/forward-white.png'
+import settingsIcon from '../../assets/images/settings-white.png'
+import BellSound from '../../assets/sounds/bellsound.mp3'
+import Modal from '../utilities/Modal'
+const bellMusic = new Audio(BellSound)
+let tiempoDeInicio = Date.now()
 const Clock = ({ pomodoroDuration = 25, restDuration = 5, addNewToRegister, textLanguage }: any) => {
-  let tiempoRestanteEnMs = pomodoroDuration * (60 * 1000);
-  const [pomodoroTime, setPomodoroTimer] = useState(pomodoroDuration);
-  const [restTime, setRestTimer] = useState(restDuration);
-  const [fin, setFin] = useState(tiempoDeInicio + tiempoRestanteEnMs);
-  const [tiempoRestante, setTiempoRestante] = useState(tiempoRestanteEnMs);
-  const [isPlaying, setPlaying] = useState(false);
-  const [resting, setResting] = useState(false);
-  const [modalShow, setShow] = useState("hidden");
+  let tiempoRestanteEnMs = pomodoroDuration * (60 * 1000)
+  const [pomodoroTime, setPomodoroTimer] = useState(pomodoroDuration)
+  const [restTime, setRestTimer] = useState(restDuration)
+  const [fin, setFin] = useState(tiempoDeInicio + tiempoRestanteEnMs)
+  const [tiempoRestante, setTiempoRestante] = useState(tiempoRestanteEnMs)
+  const [isPlaying, setPlaying] = useState(false)
+  const [resting, setResting] = useState(false)
+  const [modalShow, setShow] = useState('hidden')
   const calcularNuevoFinal = (fTimpo: number): number => {
-    return fTimpo - Date.now();
-  };
+    return fTimpo - Date.now()
+  }
 
   const openModal = () => {
-    setResting(false);
-    setPlaying(false);
-    setShow("block");
-  };
+    setResting(false)
+    setPlaying(false)
+    setShow('block')
+  }
   const closeShow = () => {
-    setShow("hidden");
-  };
+    setShow('hidden')
+  }
   const pause = () => {
-    setFin(Date.now() + tiempoRestante);
-    setPlaying(!isPlaying);
-  };
+    setFin(Date.now() + tiempoRestante)
+    setPlaying(!isPlaying)
+  }
 
   const stop = () => {
-    setPlaying(false);
-    setTiempoRestante(pomodoroTime * 60 * 1000);
-    setResting(false);
-  };
+    setPlaying(false)
+    setTiempoRestante(pomodoroTime * 60 * 1000)
+    setResting(false)
+  }
 
   const setPomodoroTime = (event: any) => {
-    setPlaying(false);
-    if (event.target.value > 0) setPomodoroTimer(parseFloat(event.target.value));
-    else setPomodoroTime(25);
-    setResting(false);
-    setTiempoRestante(pomodoroTime * 60 * 1000);
-  };
+    setPlaying(false)
+    if (event.target.value > 0) setPomodoroTimer(parseFloat(event.target.value))
+    else setPomodoroTime(25)
+    setResting(false)
+    setTiempoRestante(pomodoroTime * 60 * 1000)
+  }
 
   const setRestTime = (event: any) => {
-    setPlaying(false);
-    if (event.target.value > 0) setRestTimer(parseFloat(event.target.value));
-    else setPomodoroTime(25);
-    setResting(false);
-    setTiempoRestante(pomodoroTime * 60 * 1000);
-  };
+    setPlaying(false)
+    if (event.target.value > 0) setRestTimer(parseFloat(event.target.value))
+    else setPomodoroTime(25)
+    setResting(false)
+    setTiempoRestante(pomodoroTime * 60 * 1000)
+  }
 
   const endPomodoro = () => {
     if (tiempoRestante <= 1000) {
-      pause();
-      setResting(!resting);
-      setTiempoRestante(pomodoroTime * 60 * 1000);
-      bellMusic.play();
+      pause()
+      setResting(!resting)
+      setTiempoRestante(pomodoroTime * 60 * 1000)
+      bellMusic.play()
       if (!resting) {
-        setTiempoRestante(restTime * 60 * 1000);
-        addNewToRegister();
+        setTiempoRestante(restTime * 60 * 1000)
+        addNewToRegister()
       }
     }
-  };
+  }
 
   useEffect(() => {
-    const updateTime = 500;
+    const updateTime = 500
     let tTimer = setTimeout(() => {
       if (isPlaying) {
-        setTiempoRestante(calcularNuevoFinal(fin));
-        endPomodoro();
+        setTiempoRestante(calcularNuevoFinal(fin))
+        endPomodoro()
       }
-    }, updateTime);
+    }, updateTime)
     return () => {
-      clearTimeout(tTimer);
-    };
-  }, [tiempoRestante, isPlaying]);
+      clearTimeout(tTimer)
+    }
+  }, [tiempoRestante, isPlaying])
 
-  let minutesRemaining = Math.floor(tiempoRestante / 60 / 1000);
-  let secondsRemaining = Math.floor((tiempoRestante / 1000) % 60);
+  let minutesRemaining = Math.floor(tiempoRestante / 60 / 1000)
+  let secondsRemaining = Math.floor((tiempoRestante / 1000) % 60)
 
   const pauseOrPlay = () => {
-    if (isPlaying) return <Icon img={pauseImage} />;
-    return <Icon img={playImage} />;
-  };
+    if (isPlaying) return <Icon img={pauseImage} />
+    return <Icon img={playImage} />
+  }
 
   useEffect(() => {
-    setPlaying(false);
-    setPomodoroTimer(pomodoroTime);
-    setTiempoRestante(pomodoroTime * 60 * 1000);
-  }, [pomodoroTime]);
+    setPlaying(false)
+    setPomodoroTimer(pomodoroTime)
+    setTiempoRestante(pomodoroTime * 60 * 1000)
+  }, [pomodoroTime])
 
   useEffect(() => {
-    setPlaying(false);
-    setRestTimer(restTime);
-    setResting(false);
-  }, [restTime]);
+    setPlaying(false)
+    setRestTimer(restTime)
+    setResting(false)
+  }, [restTime])
 
   return (
     <div className="text-white text-center ">
       <div
         className={`border-2 align-middle rounded-full m-auto w-56 h-56 lg:w-96  lg:h-96 flex justify-center ${
-          resting ? "bg-green-600" : "bg-red-600"
+          resting ? 'bg-green-600' : 'bg-red-600'
         }`}
       >
         <p className="text-6xl m-auto ">
@@ -117,17 +117,17 @@ const Clock = ({ pomodoroDuration = 25, restDuration = 5, addNewToRegister, text
         </p>
       </div>
       <Button name={pauseOrPlay()} action={pause} bgColor="bg-black" />
-      <Button name={<Icon img={fastForward} />} action={stop} extraStyle={"ml-2"} bgColor="bg-black" />
+      <Button name={<Icon img={fastForward} />} action={stop} extraStyle={'ml-2'} bgColor="bg-black" />
       <Button
         name={<Icon img={settingsIcon} />}
-        size={"w-24 h-24"}
-        extraStyle={"ml-2"}
+        size={'w-24 h-24'}
+        extraStyle={'ml-2'}
         action={openModal}
         bgColor="bg-black"
       />
       <Modal show={modalShow} updateShow={closeShow}>
         <div className="text-black">
-          <label htmlFor="">{textLanguage.cant || "place"}</label>
+          <label htmlFor="">{textLanguage.cant || 'place'}</label>
           <input
             type="number"
             className="bg-gray-200  text-center rounded-md block w-full "
@@ -136,7 +136,7 @@ const Clock = ({ pomodoroDuration = 25, restDuration = 5, addNewToRegister, text
             placeholder="25"
             max={999}
           />
-          <label htmlFor="">{textLanguage.restCant || "place"}</label>
+          <label htmlFor="">{textLanguage.restCant || 'place'}</label>
           <input
             type="number"
             className="bg-gray-200  text-center rounded-md block w-full "
@@ -148,7 +148,7 @@ const Clock = ({ pomodoroDuration = 25, restDuration = 5, addNewToRegister, text
         </div>
       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default Clock;
+export default Clock
--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,48 +1,48 @@
-import Clock from "../clock/Clock";
-import { useState, useEffect } from "react";
-import RegisterRenderer from "../RegisterRenderer";
-import jsonEn from "../../assets/text/homeEn.json";
-import jsonEs from "../../assets/text/homeEs.json";
-import Modal from "../utilities/Modal";
-let userLang = navigator.language;
-let textLanguage: any;
+import Clock from '../clock/Clock'
+import { useState, useEffect } from 'react'
+import RegisterRenderer from '../RegisterRenderer'
+import jsonEn from '../../assets/text/homeEn.json'
+import jsonEs from '../../assets/text/homeEs.json'
+
+let userLang = navigator.language
+let textLanguage: any
 if (userLang.substring(0, 2) === `es`) {
-  textLanguage = jsonEs;
+  textLanguage = jsonEs
 } else {
-  textLanguage = jsonEn;
+  textLanguage = jsonEn
 }
 function Home() {
-  const [register, setRegister] = useState<any>([]);
-  const [tPomodoro, setTPomodoro] = useState<any>(25);
-  const [tRest, setTRest] = useState<any>(5);
+  const [register, setRegister] = useState<any>([])
+  const [tPomodoro, setTPomodoro] = useState<any>(25)
+  const [tRest, setTRest] = useState<any>(5)
   const addPomodoroRegister = () => {
-    const date = new Date();
-    const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-    const hour = `${date.getHours()}:${date.getMinutes()}`;
+    const date = new Date()
+    const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+    const hour = `${date.getHours()}:${date.getMinutes()}`
     const newRegister = {
       today,
-      hour,
-    };
-    setRegister([...register, newRegister]);
-    let oldItems = JSON.parse(localStorage.getItem("register") || "[]");
-    oldItems.push(newRegister);
-    localStorage.setItem("register", JSON.stringify(oldItems));
-  };
+      hour
+    }
+    setRegister([...register, newRegister])
+    let oldItems = JSON.parse(localStorage.getItem('register') || '[]')
+    oldItems.push(newRegister)
+    localStorage.setItem('register', JSON.stringify(oldItems))
+  }
 
   useEffect(() => {
-    const oldPomodoros = localStorage.getItem("register") || "empty";
+    const oldPomodoros = localStorage.getItem('register') || 'empty'
 
-    if (oldPomodoros != "empty") {
-      let registrosPomodoro = JSON.parse(oldPomodoros);
-      setRegister(registrosPomodoro);
+    if (oldPomodoros != 'empty') {
+      let registrosPomodoro = JSON.parse(oldPomodoros)
+      setRegister(registrosPomodoro)
     }
-  }, []);
+  }, [])
   const saveValues = () => {
-    return;
-  };
+    return
+  }
   useEffect(() => {
     //
-  }, [tPomodoro, tRest]);
+  }, [tPomodoro, tRest])
 
   return (
     <div className="bg-slate-700 h-fit min-h-screen  flex items-center justify-center md:flex-row font-ubuntu flex-col-reverse">
@@ -58,7 +58,7 @@ function Home() {
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
